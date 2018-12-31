3434e1b767ce5f058424f6fbf72f812c0adc12c6
unknown <misti.mechelle@gmail.com>
2018-12-30 23:54:03 -0600

    update routing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -20,5 +20,34 @@ module.exports = function(app) {
             scoreDifference: Infinity
         };
         
-    });
-}
+
+    var userData = req.body;
+    var userScores = userData.scores;
+
+    var totalDifference;
+
+    for (var i = 0; i < friendsData.length; i++) {
+        var currentFriend = friends[i];
+        totalDifference =0;
+
+        console.log(currentFriend.name);
+
+        for (var j = 0; j < currentFriend.scores.length; j++) {
+            var currentFriendScore = currentFriend.scores[j];
+            var curentUserScore = userScores[j];
+
+            totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+        }
+        if (totalDifference <= bestMatch.friendDifference) {
+            bestMatch.name = currentFriend.name;
+            bestMatch.photo = currentFriend.photo;
+            bestMatch.scoreDifference = totalDifference;
+        }
+    }
+    friends.push(userData);
+
+    res.json(bestMatch);
+
+});
+
+};
